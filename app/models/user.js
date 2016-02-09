414d255ae694e42fe62bc232ec96b4b06153f156
boyajay <boyajay@github.com>
2016-02-08 16:01:46 -0800

    Add user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,27 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var Link = require('./link.js');
 
 
 
 var User = db.Model.extend({
+	tableName: 'users',
+	hasTimestamps: true,
+	
+	links: function () {
+		return this.hasMany(Link);
+	},
+
+	encryptPass: function(pass){
+		var salt = bcrypt.genSaltSync(10);
+		var hash = bcrypt.hashSync(pass, salt);
+
+		// call func to store salt
+		// call func to store hash
+	}	
+
+
 });
 
 module.exports = User;
